5e91567eeb59f560a1cdd3d70994ee64f680b81a
Murilo Prado <108947811+murilo-py@users.noreply.github.com>
2026-06-22 15:57:41 -0400

    Update bee1199.py

--- Exercicios/bee1199.py
+++ Exercicios/bee1199.py
@@ -7,3 +7,49 @@ A entrada contém vários casos de teste. Cada linha de entrada, com exceção d
 Saída
 Para cada linha de entrada (exceto a última) deve ser produzido uma linha de saída. Todo número hexadecimal deve ser precedido na saída por '0x' (zero xis).
 '''
+valores_hexa = "0123456789ABCDEF"
+
+while True:
+
+    linha = input().strip()
+    
+    if linha[0] == '-':
+        break
+
+    if len(linha) > 1 and (linha[1] == 'x' or linha[1] == 'X'):
+        hexa = linha[2:].upper()
+        
+        decimal = 0
+        
+        hexa_invertido = hexa[::-1]
+        
+        
+        for i in range(len(hexa_invertido)):
+            digito = hexa_invertido[i]
+            
+            valor_digito = valores_hexa.index(digito)
+            
+            
+            decimal += valor_digito * (16 ** i)
+            
+        print(decimal)
+
+
+    else:
+        numero = int(linha)
+        
+        
+        if numero == 0:
+            print("0x0")
+            continue
+            
+        resultado_hexa = ""
+
+        while numero > 0:
+            resto = numero % 16
+            resultado_hexa += valores_hexa[resto] 
+            numero = numero // 16                 
+
+        resultado_hexa = resultado_hexa[::-1]
+        
+        print("0x" + resultado_hexa)
